cfa615ccf59f8166b4c910cb5eb9ff1d5a604624
austinsamsel <austinsamsel@gmail.com>
2015-04-02 23:14:00 -0400

    add b-lazy and replace unveil

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,7 @@
 // source/js/all.js
 //= require jquery/dist/jquery
 //= require fullpage.js/jquery.fullPage
-//= require jquery-unveil/jquery.unveil
+
 //= require waypoints/lib/jquery.waypoints
 //= require typed.js/js/typed
+//= require blazy.min.js
