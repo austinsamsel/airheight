c5eacecd3a2af5b65d3b82a7a8747da1ed94a526
austinsamsel <austinsamsel@gmail.com>
2015-04-15 21:07:49 -0400

    build

--- source/javascripts/layout_blog.js
+++ source/javascripts/layout_blog.js
@@ -14,4 +14,30 @@ var navMenu = function(){
 
 $( document ).ready(function() {
   navMenu();
+
+  $('form').garlic();
+
+  var $contactForm = $('form');
+  $contactForm.submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: '//formspree.io/hi@hightopsnyc.com',
+      method: 'POST',
+      data: $(this).serialize(),
+      dataType: 'json',
+      beforeSend: function() {
+        $contactForm.find('.alert--success').hide();
+        $contactForm.find('.alert--error').hide();
+        $contactForm.append('<div class="alert alert--loading"><div class="ui active inline loader small-loader"></div> Sending application…</div>');
+      },
+      success: function(data) {
+        $contactForm.find('.alert--loading').hide();
+        $contactForm.append('<div class="alert alert--success"><i class="fa fa-check green"></i> Application sent!</div>');
+      },
+      error: function(err) {
+        $contactForm.find('.alert--loading').hide();
+        $contactForm.append(' <div class="alert alert--error"><i class="fa fa-remove red"></i> Oops, there was an error.</div>');
+      }
+    });
+  });
 });
