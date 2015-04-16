6a6b6f358ae45826f5e43b0f4e6fe1f07206986b
austinsamsel <austinsamsel@gmail.com>
2015-04-15 23:56:22 -0400

    application form doesnt work. updated analytics on upstate

--- source/javascripts/layout_blog.js
+++ source/javascripts/layout_blog.js
@@ -1,46 +1,46 @@
 $( document ).ready(function() {
 
-var navMenu = function(){
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+  var navMenu = function(){
+    var menuToggle = $('#js-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass("show");
 
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
-      }
+    menuToggle.on('click', function(e) {
+      e.preventDefault();
+      $('#js-navigation-menu').slideToggle(function(){
+        if($('#js-navigation-menu').is(':hidden')) {
+          $('#js-navigation-menu').removeAttr('style');
+        }
+      });
     });
-  });
-};
+  };
 
   navMenu();
 
-  $('form').garlic();
+  $('#upStateApp').garlic();
 
-  // var $contactForm = $('#upStateApp');
-  // $contactForm.submit(function(e) {
-  //   e.preventDefault();
-  //   $.ajax({
-  //     url: '//formspree.io/hi@hightopsnyc.com',
-  //     method: 'POST',
-  //     data: $(this).serialize(),
-  //     dataType: 'json',
-  //     beforeSend: function() {
-  //       $contactForm.find('.alert--success').hide();
-  //       $contactForm.find('.alert--error').hide();
-  //       $contactForm.append('<div class="alert alert--loading"><div class="ui active inline loader small-loader"></div> Sending message…</div>');
-  //     },
-  //     success: function(data) {
-  //       $contactForm.find('.alert--loading').hide();
-  //       $contactForm.append('<div class="alert alert--success"><i class="fa fa-check green"></i> Message sent!</div>');
-  //     },
-  //     error: function(err) {
-  //       $contactForm.find('.alert--loading').hide();
-  //       $contactForm.append(' <div class="alert alert--error"><i class="fa fa-remove red"></i> Oops, there was an error.</div>');
-  //     }
-  //   });
-  // });
+  var $contactForm = $('#upStateApp');
+  $contactForm.submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: '//formspree.io/hi@hightopsnyc.com',
+      method: 'POST',
+      data: $(this).serialize(),
+      dataType: 'json',
+      beforeSend: function() {
+        $contactForm.find('.alert--success').hide();
+        $contactForm.find('.alert--error').hide();
+        $contactForm.append('<div class="alert alert--loading"><div class="ui active inline loader small-loader"></div> Sending message…</div>');
+      },
+      success: function(data) {
+        $contactForm.find('.alert--loading').hide();
+        $contactForm.append('<div class="alert alert--success"><i class="fa fa-check green"></i> Message sent!</div>');
+      },
+      error: function(err) {
+        $contactForm.find('.alert--loading').hide();
+        $contactForm.append(' <div class="alert alert--error"><i class="fa fa-remove red"></i> Oops, there was an error.</div>');
+      }
+    });
+  });
 
-  
+
 });
