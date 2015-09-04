57319981e8957b39c7453f026ae20cef44d0cc4a
austin samsel <austinsamsel@gmail.com>
2015-09-03 23:38:21 -0400

    build

---
+++ tmp/bower_components/semantic-ui-loader/package-4556f237.js
@@ -0,0 +1,1 @@
+Package.describe({name:"semantic:ui-loader",summary:"Semantic UI - Loader: Single component release",version:"1.12.3",git:"git://github.com/Semantic-Org/UI-Loader.git"}),Package.onUse(function(e){e.versionsFrom("1.0"),e.addFiles(["loader-a5991671.css"],"client")});
